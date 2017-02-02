c5a113e94c88e62ac5e4b67c2ca513e8216c41b3
Simon Powell <simon@powellmail.co.uk>
2017-02-02 17:55:56 +0000

    Added add vessel

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Battleships
 Well, doh.
+
+#	Class of ship	Size
+1	Carrier	5
+2	Battleship	4
+3	Cruiser	3
+4	Submarine	3
+5	Destroyer	2
+
--- battleships.py
+++ battleships.py
@@ -1,6 +1,7 @@
 """Battleships
 """
 
+
 class sea:
 
     def __init__(self):
@@ -8,7 +9,7 @@ class sea:
         self.water = [[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
                       [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
                       [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-                      [' ', ' ', ' ', 'W', ' ', ' ', ' ', ' ', ' ', ' '],
+                      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
                       [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
                       [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
                       [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
@@ -26,8 +27,29 @@ class sea:
             print ''
             print '-------------------------------'
 
+    def add_vessel(self, type, location, orientation):
+
+        if type is 'C':
+            vessel_length = 5
+        elif type is 'B':
+            vessel_length = 4
+        elif type is 'R':
+            vessel_length = 3
+        elif type is 'S':
+            vessel_length = 3
+        elif type is 'D':
+            vessel_length = 2
+
+        if orientation is 'E':
+            for h in range(location[0], location[0] + vessel_length):
+                self.water[h][location[1]] = type
+        elif orientation is 'S':
+            for v in range(location[1], location[1] + vessel_length):
+                self.water[location[0]][v] = type
+
 
 p1_board = sea()
 
-p1_board.display()
+p1_board.add_vessel('C', [3,3], 'S')
 
+p1_board.display()
